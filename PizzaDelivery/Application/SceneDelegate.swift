dce932ca4361c045e0c57186d36c45e8efa22a1e
deshabml <deshab@yandex.ru>
2024-01-13 09:43:12 +0300

    Start Projekt

---
+++ PizzaDelivery/Application/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  PizzaDelivery
+//
+//  Created by Лаборатория on 13.01.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let winScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: winScene)
+        window.overrideUserInterfaceStyle = .light
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window    }
+
+}
+
